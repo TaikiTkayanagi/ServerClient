4c21bad1350fcd4c9e0b897f57b35093022659e9
TaikiTkayanagi <ikiat4@gmail.com>
2021-12-15 00:13:55 +0900

    Jsonを受け取るcontrollerの作成

---
+++ Server/SpringBoot/src/main/java/spring/rest/demo/Controller/JsonCatchController.java
@@ -0,0 +1,24 @@
+package spring.rest.demo.Controller;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import spring.rest.demo.Data.Information;
+
+@RestController
+public class JsonCatchController {
+
+    org.slf4j.Logger logger = LoggerFactory.getLogger("BridgestoneController");
+
+    @PostMapping("/")
+    @ResponseBody
+    public Information getJson(@RequestBody Information information, Model model){
+        logger.info("Json is {}", information.toString());
+
+        return information;
+    }
+}

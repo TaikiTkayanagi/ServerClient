220b5742808c79d7a3676cc43ffc154b4ec585ed
TaikiTkayanagi <ikiat4@gmail.com>
2022-01-05 21:26:47 +0900

    GradleでのSpringBootプロジェクトの作成

---
+++ Server/JsonCatchServer/servercatchjson/src/main/java/server/springboot/servercatchjson/controller/JsonCatchController.java
@@ -0,0 +1,31 @@
+package server.springboot.servercatchjson.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import server.springboot.servercatchjson.services.Information;
+
+
+@Controller
+public class JsonCatchController {
+    Logger logger = LoggerFactory.getLogger("JsonCatchController");
+
+    @GetMapping("/")
+    @ResponseBody
+    public String welComePage(){
+        return "welcome to JsonCatchController";
+    }
+
+    @GetMapping("/json")
+    public Information getInformationTest(@RequestBody Information information) {
+        logger.info("Json is {}", information.toString());
+        return information;
+    }
+
+}
